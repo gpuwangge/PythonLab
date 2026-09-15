5505cd23137ed1148098a8ecf903590e98c133c1
Connor <wxj995@gmail.com>
2026-09-15 16:05:57 -0700

    update database sample

--- FlaskTest/databasesample/app.py
+++ FlaskTest/databasesample/app.py
@@ -1,43 +1,89 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy  # 导入扩展类
+from sqlalchemy.orm import DeclarativeBase #实例化扩展类时，除了程序实例，需要额外传入一个继承自 DeclarativeBase 的子类作为 model_class 参数的值。
+from pathlib import Path
+from sqlalchemy import String
+from sqlalchemy.orm import Mapped, mapped_column
+import click
+from sqlalchemy import select
 
 app = Flask(__name__)
 
-#我们把包含变量和运算逻辑的 HTML 或其他格式的文本叫做模板（template）
-#执行这些变量替换和逻辑计算工作的过程被称为渲染（rendering）
-#这个工作由模板渲染引擎——Jinja2来完成
-#Jinja2要解决的问题：网页复杂后直接把大量 HTML 写进 Python 字符串会很难维护
+#写入了一个 SQLALCHEMY_DATABASE_URI 变量来告诉 SQLAlchemy 数据库连接地址
+#sqlite:///数据库文件的绝对地址
+#非Windows，sqlite:////绝对路径/数据库文件名.db
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + str(Path(app.root_path) / 'data.db')
 
-#Jinja2的语法（代码写在templates/index.html里）
-#{{ ... }} 用来标记变量。
-#{% ... %} 用来标记语句，比如 if 语句，for 语句等。
-#{# ... #} 用来写注释。
-#为了方便对变量进行处理，Jinja2 提供了一些过滤器，语法形式如下：
-#{{ 变量|过滤器 }}
+#目前这个类是空的，后续你可以按照需要对这个基类进行自定义。
+class Base(DeclarativeBase):
+  pass
 
-#定义虚拟数据
-name = 'Wangge'
-movies = [
-    {'title': 'My Neighbor Totoro', 'year': '1988'},
-    {'title': 'Dead Poets Society', 'year': '1989'},
-    {'title': 'A Perfect World', 'year': '1993'},
-    {'title': 'Leon', 'year': '1994'},
-    {'title': 'Mahjong', 'year': '1996'},
-    {'title': 'Swallowtail Butterfly', 'year': '1996'},
-    {'title': 'King of Comedy', 'year': '1999'},
-    {'title': 'Devils on the Doorstep', 'year': '1999'},
-    {'title': 'WALL-E', 'year': '2008'},
-    {'title': 'The Pork of Music', 'year': '2012'},
-]
+#初始化扩展，传入程序实例 app
+db = SQLAlchemy(app, model_class=Base)  
+
+class User(db.Model):
+    __tablename__ = 'user' # 定义表名称
+    id: Mapped[int] = mapped_column(primary_key=True)  # 主键
+    name: Mapped[str] = mapped_column(String(20))  # 名字
+
+
+class Movie(db.Model):  # 表名将会是 movie
+    __tablename__ = 'movie'
+    id: Mapped[int] = mapped_column(primary_key=True)  # 主键
+    title: Mapped[str] = mapped_column(String(60))  # 电影标题
+    year: Mapped[str] = mapped_column(String(4))  # 电影年份
+
+#生成数据库，或者删除数据库
+#python -m flask init-db
+#python -m flask init-db --drop
+@app.cli.command('init-db')  # 注册为命令，传入自定义命令名
+@click.option('--drop', is_flag=True, help='Create after drop.')  # 设置选项
+def init_database(drop):
+    """Initialize the database."""
+    if drop:  # 判断是否输入了选项
+        db.drop_all()
+    db.create_all()
+    click.echo('Initialized database.')  # 输出提示信息
+
+#伪造数据库数据并重新生成数据库
+#python -m flask forge
+@app.cli.command()
+def forge():
+    """Generate fake data."""
+    db.drop_all()
+    db.create_all()
+
+    # 全局的两个变量移动到这个函数内
+    name = 'Daniel'
+    movies = [
+        {'title': 'My Neighbor Totoro', 'year': '1988'},
+        {'title': 'Dead Poets Society', 'year': '1989'},
+        {'title': 'A Perfect World', 'year': '1993'},
+        {'title': 'Leon', 'year': '1994'},
+        {'title': 'Mahjong', 'year': '1996'},
+        {'title': 'Swallowtail Butterfly', 'year': '1996'},
+        {'title': 'King of Comedy', 'year': '1999'},
+        {'title': 'Devils on the Doorstep', 'year': '1999'},
+        {'title': 'WALL-E', 'year': '2008'},
+        {'title': 'The Pork of Music', 'year': '2012'},
+    ]
+
+    user = User(name=name)
+    db.session.add(user)
+    for m in movies:
+        movie = Movie(title=m['title'], year=m['year'])
+        db.session.add(movie)
+
+    db.session.commit()
+    click.echo('Done.')
 
 #http://127.0.0.1:5000/
-#在传入render_template()函数的关键字参数中，左边的 movies 是模板中使用的变量名称，右边的 movies 则是该变量指向的实际对象。
-#这里传入模板的 name 是字符串，movies 是列表
-#render_template()函数在调用时会识别并执行 index.html 里所有的 Jinja2 语句
-#在返回的页面中，变量会被替换为实际的值（包括定界符），语句（及定界符）则会在执行后被移除（注释也会一并移除）。
-@app.route("/")
+#@app.route("/")
+#def index():
+#    return render_template('index.html', name=name, movies=movies)
+
+@app.route('/')
 def index():
-    return render_template('index.html', name=name, movies=movies)
-
-
-
-
+    user = db.session.execute(select(User)).scalar()  # 读取用户记录
+    movies = db.session.execute(select(Movie)).scalars().all()  # 读取所有电影记录
+    return render_template('index.html', user=user, movies=movies)
